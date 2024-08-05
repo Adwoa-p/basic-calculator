e4a2129407a9d49df1fb9ae48864f819ff7cb045
owuraku-zenas <zenasawuku@gmail.com>
2024-08-05 17:38:01 +0000

    Added exceptions

--- Basic_Calculator.py
+++ Basic_Calculator.py
@@ -1,21 +1,22 @@
 while True:
-    a=int(input("Enter a value: "))
-    b=int(input("Enter a value: "))
-    operation=input("Input the kind of operation you want to perform(sum,product,difference or division): ")
-    if operation== "sum":
-        print(a+b)
-    elif operation== "product":
-        print(a*b)
-    elif operation== "difference":
-        print(a-b)
-    elif operation== "division":
-        if b!=0:
-               print(a/b)
-        else:
-            print("Math Error")
-    prompt=input("Enter in to continue or out to exit: ")
-    if prompt== "in":
-        continue
-    elif prompt== "out":
-        break
+    try:
+        a=int(input("Enter First Number: "))
+        b=int(input("Enter Second Number: "))
+        operation=input("Input the kind of operation you want to perform(sum,product,difference or division): ")
+        if operation== "sum":
+            print(a+b)
+        elif operation== "product":
+            print(a*b)
+        elif operation== "difference":
+            print(a-b)
+        elif operation== "division":
+            print(a/b)
+        
+        prompt=input("Enter in to continue or out to exit: ")
+        if prompt== "in":
+            continue
+        elif prompt== "out":
+            break
+    except ValueError as error:
+        print(error)
     
